96fa1cd5628d4721c92180d2caec868ac4939f66
unknown <Auingere22019@sus.htl-grieskirchen.at>
2024-09-17 11:12:35 +0200

    Interface completed

--- HUE1/src/net/htlgkr/pos3/auingere/PrimeSieve.java
+++ HUE1/src/net/htlgkr/pos3/auingere/PrimeSieve.java
@@ -1,4 +1,8 @@
 package net.htlgkr.pos3.auingere;
 
 public interface PrimeSieve {
+    //Ueberprueft ob p eine Primzahl ist oder nicht.
+    public boolean isPrime(int p);
+    //Gibt alle Primzahlen bis zur Obergrenze auf der Konsole aus.
+    public void printPrimes();
 }
