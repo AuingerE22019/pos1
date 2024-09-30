24e00bce36f5a6085d0b16f16b9f720050725617
AuingerE22019 <AuingerE22019@sus.htl-grieskirchen.at>
2024-09-30 21:30:18 +0200

    Completed Class "HalloJavamitForEach"

--- 2ndHUE/src/HalloJavamitForEach.java
+++ 2ndHUE/src/HalloJavamitForEach.java
@@ -1,2 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class HalloJavamitForEach {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Hallo", "Java", "mit", "ForEach");
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        list.forEach(System.out::println);
+    }
 }
