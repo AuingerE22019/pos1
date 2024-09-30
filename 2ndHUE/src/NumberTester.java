c89459e50c2a984ed9f967402e7dc7ad02b105cd
AuingerE22019 <AuingerE22019@sus.htl-grieskirchen.at>
2024-09-30 21:34:58 +0200

    Structured Class "NumberTester"

--- 2ndHUE/src/NumberTester.java
+++ 2ndHUE/src/NumberTester.java
@@ -1,3 +1,27 @@
-public class NumberTester {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class NumberTester {
+    //...
+
+    public NumberTester(String fileName) {
+        //TODO
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() {
+        //TODO
+    }
 }
