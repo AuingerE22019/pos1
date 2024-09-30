e015afff4ecf365eb37b0455282fb94be131028c
AuingerE22019 <AuingerE22019@sus.htl-grieskirchen.at>
2024-09-30 21:49:03 +0200

    Implemented Method "testFile()" in Class "NumberTester"

--- 2ndHUE/src/NumberTester.java
+++ 2ndHUE/src/NumberTester.java
@@ -25,7 +25,40 @@ public class NumberTester implements NumberTest{
     }
 
     public void testFile() {
-        //TODO
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line = br.readLine();
+            int numberOfTests = Integer.parseInt(line);
+
+            for (int i = 0; i < numberOfTests; i++) {
+                line = br.readLine();
+                String[] parts = line.split(" ");
+                int testType = Integer.parseInt(parts[0]);
+                int number = Integer.parseInt(parts[1]);
+
+                switch (testType) {
+                    case 1 -> {
+                        if (oddTester != null) {
+                            boolean result = oddTester.testNumber(number);
+                            System.out.println(result ? "EVEN" : "ODD");
+                        }
+                    }
+                    case 2 -> {
+                        if (primeTester != null) {
+                            boolean result = primeTester.testNumber(number);
+                            System.out.println(result ? "PRIME" : "NO PRIME");
+                        }
+                    }
+                    case 3 -> {
+                        if (palindromeTester != null) {
+                            boolean result = palindromeTester.testNumber(number);
+                            System.out.println(result ? "PALINDROME" : "NO PALINDROME");
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
